561c12ae7dbf9b6d31cd97b4d8118d3cc2833465
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2025-04-22 18:13:31 +0530

    Update Admin.jsx

--- hotelmangt/src/components/admin/Admin.jsx
+++ hotelmangt/src/components/admin/Admin.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Admin = () => {
   return (
-    <section className='container mt-5'>
+    <section className='container mt-5' style={{height:'530px'}}>
       <h2>Welcome to Admin Panel</h2>
       <hr/>
 
