7b780b2145def02c0b75aea1b2cd953927ae49f2
namratachavan15 <141033807+namratachavan15@users.noreply.github.com>
2026-04-01 18:32:58 +0530

    Update Header.jsx

--- hotelmangt/src/components/common/Header.jsx
+++ hotelmangt/src/components/common/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = ({title}) => {
   return (
-    <header className='header'>
+    <header className='header' style={{marginTop:'50px'}} >
       <div className='overlay'></div>
       <div className='container'>
         <h1 className='header-title text-center'>
